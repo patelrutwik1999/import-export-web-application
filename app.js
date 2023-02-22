4a93d28bbdef0cef2155e8b2bba670acca5ecb2c
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-02-21 21:50:36 -0500

    Form/form-advanced setup done

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
+var i18n = require("i18n-express");
+var flash = require('connect-flash');
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+var expressLayouts = require('express-ejs-layouts');
 
 const app = express()
+
+//MiddleWares
+app.use(morgan('dev'))
+app.use(bodyParser.json());
+var urlencodeParser = bodyParser.urlencoded({ extended: true });
+app.use(flash())
+app.use(expressLayouts);
+
+
+//For different language translations
+app.use(i18n({
+    translationsPath: path.join(__dirname, 'i18n'), // <--- use here. Specify translations files path.
+    siteLangs: ["es", "en", "de", "ru", "it", "fr"],
+    textsVarName: 'translation'
+}));
+app.use('/public', express.static('public'));
+
+app.get('/layouts/', function (req, res) {
+    res.render('view');
+});
+
+//For set layouts of html view
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/form', function (req, res) {
+    res.locals = { title: 'Advanced Plugins' };
+    res.render('Form/form-advanced');
+});
 
 const port = process.env.PORT || 10934
 app.listen(port, () => {
